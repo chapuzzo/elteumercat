4c42b49262770648ae7420bfff5466019f2acaf5
chapuzzo <luismax@gmail.com>
2014-01-16 16:04:16 +0100

    renamed app class

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require 'rubygems'
 require File.join(File.dirname(__FILE__), 'lib/elteumercat.rb')
 
-run .
+run Elteumercat
--- lib/elteumercat.rb
+++ lib/elteumercat.rb
@@ -1,8 +1,8 @@
 require 'sinatra/base'
 
-class . < Sinatra::Base
+class Elteumercat < Sinatra::Base
   get '/' do
-    'Hello .!'
+    'Hello Elteumercat!'
   end
 
   # start the server if ruby file executed directly
